2f9cc87ffbdbcf98a1c0fde09e7ccb2119d428fd
JRudransh <JRudransh@protonmail.com>
2022-01-23 01:05:15 +0530

    Started updating

--- vars.py
+++ vars.py
@@ -1,15 +1,5 @@
 # Set your main path
 
-# Windows
-# my_target = "c:/"
-
-# Linux
-my_target = "/root"
-
-
-# To manage downloads only
-# my_folder_list = ["Downloads"]
-
 # To manage all
 my_folder_list = ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Videos"]
 
